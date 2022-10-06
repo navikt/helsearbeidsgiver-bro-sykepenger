fce290be0728d724d873c05d55c0291b692a13ac
Gustav Berggren <gustav.berggren@nav.no>
2022-10-06 10:08:03 +0200

    logg melding ved startup

--- src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/App.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/App.kt
@@ -1,5 +1,8 @@
 package no.nav.helsearbeidsgiver.bro.sykepenger
 
+import org.slf4j.LoggerFactory
+
 fun main() {
-    // Logg hello bro! i loggene
+    val broLogger = LoggerFactory.getLogger("BroLogger")
+    broLogger.info("Hello bro!")
 }
