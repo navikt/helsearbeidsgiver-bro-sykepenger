f9368095c75072128fbc744e2d71669edbbc376a
Gustav Berggren <83211434+b162214@users.noreply.github.com>
2022-10-06 10:15:16 +0200

    Hello bro applikasjon (#1)
    opprett hello bro applikasjon og deploy til dev-gcp

---
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/App.kt
@@ -0,0 +1,8 @@
+package no.nav.helsearbeidsgiver.bro.sykepenger
+
+import org.slf4j.LoggerFactory
+
+fun main() {
+    val broLogger = LoggerFactory.getLogger("BroLogger")
+    broLogger.info("Hello bro!")
+}
