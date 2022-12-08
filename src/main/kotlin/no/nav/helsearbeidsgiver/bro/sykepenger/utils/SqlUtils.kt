ad9440f09790567b83b4d2c528a422b983071ce4
Mikael Bjerga <mikael.bjerga@nav.no>
2022-12-08 12:42:08 +0100

    Ikke bruk star projections
    Co-authored-by: Gustav Berggren <gustav.berggren@nav.no>
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/utils/SqlUtils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/utils/SqlUtils.kt
@@ -7,23 +7,23 @@ import kotliquery.queryOf
 import kotliquery.sessionOf
 import javax.sql.DataSource
 
-fun String.updateAndReturnGeneratedKey(params: Map<String, *>, dataSource: DataSource): Long? =
+fun String.updateAndReturnGeneratedKey(params: Map<String, Any?>, dataSource: DataSource): Long? =
     runQuery(params, dataSource, returnGeneratedKey = true) {
         asUpdateAndReturnGeneratedKey
     }
 
-fun String.execute(params: Map<String, *>, dataSource: DataSource): Boolean =
+fun String.execute(params: Map<String, Any>, dataSource: DataSource): Boolean =
     runQuery(params, dataSource) {
         asExecute
     }
 
-fun <T : Any> String.listResult(params: Map<String, *>, dataSource: DataSource, transform: Row.() -> T): List<T> =
+fun <T : Any> String.listResult(params: Map<String, Any>, dataSource: DataSource, transform: Row.() -> T): List<T> =
     runQuery(params, dataSource) {
         map { transform(it) }
             .asList
     }
 
-fun <T : Any> String.nullableResult(params: Map<String, *>, dataSource: DataSource, transform: Row.() -> T): T? =
+fun <T : Any> String.nullableResult(params: Map<String, Any>, dataSource: DataSource, transform: Row.() -> T): T? =
     runQuery(params, dataSource) {
         map { transform(it) }
             .asSingle
