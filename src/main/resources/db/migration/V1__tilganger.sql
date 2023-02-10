94c085997efbb3f1b8e04e3dd7d4c98d003f2031
Mikael Bjerga <mikael.bjerga@nav.no>
2023-02-10 17:27:21 +0100

    Fiks tilgang til flyways historikktabell

--- src/main/resources/db/migration/V1__tilganger.sql
+++ src/main/resources/db/migration/V1__tilganger.sql
@@ -4,6 +4,7 @@ $$
         IF EXISTS
             (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
         THEN
+            GRANT ALL PRIVILEGES ON TABLE public.flyway_schema_history TO cloudsqliamuser;
             ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO cloudsqliamuser;
             ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO cloudsqliamuser;
         END IF;
