8d8bf7472074d9174866aed89a30ab017765af0a
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-06-05 10:05:14 +0200

    Fjern forslag til AGP (#5)

--- src/test/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/testutils/MockUtils.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/testutils/MockUtils.kt
@@ -62,18 +62,7 @@ fun mockForespoerselUtenForespurtDataDto(): ForespoerselDto =
 
 fun mockForespurtDataListe(): List<ForespurtDataDto> =
     listOf(
-        ArbeidsgiverPeriode(
-            forslag = listOf(
-                Periode(
-                    fom = 1.januar,
-                    tom = 10.januar
-                ),
-                Periode(
-                    fom = 15.januar,
-                    tom = 20.januar
-                )
-            )
-        ),
+        ArbeidsgiverPeriode,
         Inntekt(
             forslag = ForslagInntekt(
                 beregningsmåneder = listOf(
@@ -83,23 +72,16 @@ fun mockForespurtDataListe(): List<ForespurtDataDto> =
                 )
             )
         ),
-        Refusjon(forslag = emptyList())
+        Refusjon(
+            forslag = listOf(
+                ForslagRefusjon(17.mai, null, 13.37)
+            )
+        )
     )
 
 fun mockForespurtDataMedFastsattInntektListe(): List<ForespurtDataDto> =
     listOf(
-        ArbeidsgiverPeriode(
-            forslag = listOf(
-                Periode(
-                    fom = 1.januar,
-                    tom = 10.januar
-                ),
-                Periode(
-                    fom = 15.januar,
-                    tom = 20.januar
-                )
-            )
-        ),
+        ArbeidsgiverPeriode,
         FastsattInntekt(
             fastsattInntekt = 31415.92
         ),
