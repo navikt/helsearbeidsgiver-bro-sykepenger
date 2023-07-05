792dd58f2e4641961cfb59a5f4d57ad0bb17c518
Mikael Bjerga <mikael.bjerga@nav.no>
2023-07-05 15:31:49 +0200

    Håndter json-felter som eksplisitt er satt til 'null'
    Co-authored-by: Gustav Berggren <gustav.berggren@nav.no>

--- src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/utils/JsonUtils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/utils/JsonUtils.kt
@@ -2,9 +2,15 @@ package no.nav.helsearbeidsgiver.bro.sykepenger.utils
 
 import com.fasterxml.jackson.databind.JsonNode
 import kotlinx.serialization.json.JsonElement
+import no.nav.helse.rapids_rivers.isMissingOrNull
 import no.nav.helsearbeidsgiver.utils.json.parseJson
 
 fun JsonNode.toJsonElement(): JsonElement =
     toString().parseJson()
 
-fun JsonNode.toJsonElementOrNull(): JsonElement? = if (isMissingNode) null else toString().parseJson()
+fun JsonNode.toJsonElementOrNull(): JsonElement? =
+    if (isMissingOrNull()) {
+        null
+    } else {
+        toJsonElement()
+    }
