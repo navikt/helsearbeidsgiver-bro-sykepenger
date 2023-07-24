8c0f2b95b649a9f64f01c06c4e4186055a177adc
Mikael Bjerga <mikael.bjerga@nav.no>
2023-07-24 12:46:25 +0200

    Fiks lesing av null-json

--- src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/utils/JsonUtils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/utils/JsonUtils.kt
@@ -1,12 +1,13 @@
 package no.nav.helsearbeidsgiver.bro.sykepenger.utils
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.nullable
 import kotlinx.serialization.json.JsonElement
 import no.nav.helsearbeidsgiver.bro.sykepenger.kafkatopic.Key
 import no.nav.helsearbeidsgiver.utils.json.fromJson
 
 fun <K : Key, T : Any> K.lesOrNull(serializer: KSerializer<T>, melding: Map<K, JsonElement>): T? =
-    melding[this]?.fromJson(serializer)
+    melding[this]?.fromJson(serializer.nullable)
 
 fun <K : Key, T : Any> K.les(serializer: KSerializer<T>, melding: Map<K, JsonElement>): T =
     lesOrNull(serializer, melding)
