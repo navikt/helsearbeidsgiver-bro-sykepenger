9809ab3cc405de85cdcd4ba985f577507e2d26f6
Mehdi Zare <mehdi.zare@nav.no>
2024-06-07 13:43:11 +0200

    Logging av Exposed til secure-logger

--- src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/App.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/App.kt
@@ -12,6 +12,8 @@ fun main() {
     broLogger.info("Hello bro!")
 
     "Exposed".logger().warn("Exposed logger")
+    "Exposed".logger().info("Exposed logger")
+    "Exposed".logger().error("Exposed logger")
 
     val forespoerselDao = ForespoerselDao(Database.db)
     val priProducer = PriProducer()
