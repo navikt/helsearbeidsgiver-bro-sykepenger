da6ede825a6bc9653a652ba64c6a82a141e39d39
Magnus Sælensminde <magnus.salensminde@bekk.no>
2024-09-30 10:19:21 +0200

    Forsøk å ta vekk serializergreier som ser ubrukt ut

--- src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/domene/HentForespoerslerForVedtaksperiodeIdListeSvar.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bro/sykepenger/domene/HentForespoerslerForVedtaksperiodeIdListeSvar.kt
@@ -1,13 +1,8 @@
-@file:UseSerializers(LocalDateSerializer::class, UuidSerializer::class)
-
 package no.nav.helsearbeidsgiver.bro.sykepenger.domene
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.UseSerializers
 import kotlinx.serialization.json.JsonElement
 import no.nav.helsearbeidsgiver.bro.sykepenger.kafkatopic.pri.Pri
-import no.nav.helsearbeidsgiver.utils.json.serializer.LocalDateSerializer
-import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
 
 @Serializable
 data class HentForespoerslerForVedtaksperiodeIdListeSvar(
