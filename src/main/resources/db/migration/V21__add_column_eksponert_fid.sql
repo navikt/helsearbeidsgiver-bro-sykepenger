5f2e6eaf1530626b4ea9c12a335347175d61ecfd
Mikael Bjerga <mikael.bjerga@nav.no>
2025-04-30 12:50:25 +0200

    Bruk varchar for kolonner med kjent lengde

--- src/main/resources/db/migration/V21__add_column_eksponert_fid.sql
+++ src/main/resources/db/migration/V21__add_column_eksponert_fid.sql
@@ -2,5 +2,5 @@ ALTER TABLE forespoersel
     ADD COLUMN eksponert_forespoersel_id UUID REFERENCES forespoersel (forespoersel_id) ON DELETE RESTRICT,
     ALTER COLUMN type TYPE TEXT,
     ALTER COLUMN status TYPE TEXT,
-    ALTER COLUMN fnr TYPE TEXT,
-    ALTER COLUMN orgnr TYPE TEXT;
+    ALTER COLUMN fnr TYPE VARCHAR(11),
+    ALTER COLUMN orgnr TYPE VARCHAR(9);
